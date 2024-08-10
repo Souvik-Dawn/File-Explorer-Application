ddb45613afda48f1e5d376b45fcbb52d559494a7
Souvik-Dawn <tusudawn16@gmail.com>
2024-08-10 12:58:30 +0530

    Create Logger.h

---
+++ include/Logger.h
@@ -0,0 +1,17 @@
+LOGGER.H
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <string>
+#include <fstream>
+
+class Logger {
+public:
+    explicit Logger(const std::string& filename);
+    void log(const std::string& message);
+
+private:
+    std::ofstream logFile_;
+};
+
+#endif // LOGGER_H
